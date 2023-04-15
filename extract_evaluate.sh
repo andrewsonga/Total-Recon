c9cb487503670baba12f7ffad646b4d10defe461
Chonghyuk Song <chonghys@autobot-0-29.eth>
2023-04-14 20:23:12 -0400

    added config files for pretrained models

---
+++ extract_evaluate.sh
@@ -0,0 +1,9 @@
+gpu_id=$1
+seqname=$2
+export CXX=g++
+
+# 1) extract reconstructed object meshes and reconstructed 6-DOF root-body / camera trajectories
+bash ./extract_fgbg.sh $gpu_id $seqname
+
+# 2) evalute method on stereo-view synthesis
+basg script/render_nvs_fgbg_stereoview.sh $gpu_id $seqname
