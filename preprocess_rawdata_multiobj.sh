bfd13d9faf3b61d32e625e049c218d648c5d65b6
Chonghyuk Song <chonghys@autobot-0-29.eth>
2023-04-20 01:36:10 -0400

    added command to make tmp directories if it doesn't already exist

--- preprocess_rawdata_multiobj.sh
+++ preprocess_rawdata_multiobj.sh
@@ -1,8 +1,12 @@
 prefix=$1
 isdynamic=y        # y/n
 rootdir=raw
+tmpdir=tmp
 
-# 1) make copies of raw dataset for per-object data preprocessing
+# 1) make tmp if it doesn't exist
+mkdir -p $tmpdir
+
+# 2) make copies of raw dataset for per-object data preprocessing
 echo making copies of raw dataset for per-object data preprocessing
 rm -rf $rootdir/$prefix-human-leftcam
 rm -rf $rootdir/$prefix-human-rightcam
@@ -13,15 +17,15 @@ cp -r $rootdir/$prefix-rightcam $rootdir/$prefix-human-rightcam
 cp -r $rootdir/$prefix-leftcam $rootdir/$prefix-animal-leftcam
 cp -r $rootdir/$prefix-rightcam $rootdir/$prefix-animal-rightcam
 
-# 2) generate preprocessed data for the "human" object
+# 3) generate preprocessed data for the "human" object
 #    this version of the dataset will include masks for the human object, and densepose features extracted for the human object
 bash preprocess/preprocess_frames_dualrig.sh $prefix-human y $isdynamic
 
-# 3) generate preprocessed data for the "animal" object
+# 4) generate preprocessed data for the "animal" object
 #    this version of the dataset will include masks for the animal object, and densepose features extracted for the animal (quadruped) object
 bash preprocess/preprocess_frames_dualrig.sh $prefix-animal n $isdynamic
 
-# 4) generate preprocessed data for the "background" object
+# 5) generate preprocessed data for the "background" object
 #    by copying the contents of that of the "human" object
 #    (assumes there only one sequence i.e. 000 with given $prefix)
 bash cp_database.sh $prefix-human000-leftcam $prefix-bkgd000-leftcam
--- preprocess_rawdata_singleobj.sh
+++ preprocess_rawdata_singleobj.sh
@@ -1,6 +1,10 @@
 prefix=$1
 ishuman=$2          # y/n
 isdynamic=y         # y/n
+tmpdir=tmp
 
-# generate preprocessed data
+# 1) make tmp if it doesn't exist
+mkdir -p $tmpdir
+
+# 2) generate preprocessed data
 bash preprocess/preprocess_frames_dualrig.sh $prefix $ishuman $isdynamic
