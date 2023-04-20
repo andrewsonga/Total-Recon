50992aa4e43c19492696e9d91f42860415c63236
Chonghyuk Song <chonghys@autobot-0-29.eth>
2023-04-20 18:53:40 -0400

    included LICENSE and updated script for downloading preprocessed data

--- download_preprocessed.sh
+++ download_preprocessed.sh
@@ -1,1 +1,15 @@
-scp ndsong@kobra.ri.cmu.edu:/home/ndsong/workspace/database_humandog.tar.gz ./
+# humandog sequence
+FILEID_HUMANDOG=1OA21wk6D4Tq3ADogp_7AzZGeCxlZdlGr
+wget "https://drive.google.com/uc?export=download&id=${FILEID_HUMANDOG}&confirm=yes" -O ./database_humandog.tar.gz
+
+# humancat sequence
+#FILEID_HUMANCAT=
+#wget -O "" ./database_humancat.tar.gz
+
+# human2 sequence
+#FILEID_HUMAN2=
+#wget -O "" ./database_human2.tar.gz
+
+# dog1_v1 sequence
+#FILEID_DOG1V1=
+#wget -O "" ./database_dog1v1.tar.gz
